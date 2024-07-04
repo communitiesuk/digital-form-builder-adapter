f5c4cfd17569a33086b16d92495f5c8f9f1d5c3d
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-07-04 10:03:31 +0100

    finalization of the project structure

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-apt install git-all
+
+echo "--  Pulling git submodules into local  --"
 git pull --recurse-submodules
+
+echo "--  Building digital-form-builder locally  --"
 # shellcheck disable=SC2164
 cd digital-form-builder
 yarn
+
+echo "--  Building digital-form-builder-adapter locally  --"
 # shellcheck disable=SC2103
 cd ..
-pwd
+yarn
+
+echo "--  Copy the node modules locally into adapter  --"
 cp -r digital-form-builder/node_modules .
 cp -r digital-form-builder/designer/node_modules .
 cp -r digital-form-builder/model/node_modules .
 cp -r digital-form-builder/runner/node_modules .
-pwd
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# digital-form-builder-dluhc-adapter
+# XGov Digital Form Builder Adapter
+
+## Setup
+
+**Always run scripts from the root directory.**
