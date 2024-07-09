64455f229fe6b2af7b552d804b4344605fe10ced
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-07-09 23:39:34 +0100

    adding fixes for the devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,18 +3,14 @@
 echo "--  Pulling git submodules into local  --"
 git pull --recurse-submodules
 
+echo "--  Building digital-form-builder-adapter locally  --"
+yarn
+
 echo "--  Building digital-form-builder locally  --"
 # shellcheck disable=SC2164
 cd digital-form-builder
 yarn
 
-echo "--  Building digital-form-builder-adapter locally  --"
-# shellcheck disable=SC2103
-cd ..
-yarn
+echo "--  Building digital-form-builder designer locally  --"
+yarn model build
 
-echo "--  Copy the node modules locally into adapter  --"
-cp -r digital-form-builder/node_modules .
-cp -r digital-form-builder/designer/node_modules .
-cp -r digital-form-builder/model/node_modules .
-cp -r digital-form-builder/runner/node_modules .
