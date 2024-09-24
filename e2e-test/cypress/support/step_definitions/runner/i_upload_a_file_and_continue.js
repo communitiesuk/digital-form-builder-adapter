58445f305bef24209f749277872a10e98a4229d2
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-09-24 23:08:03 +0100

    adding new data types & data conversion logic into adaptersdfsdfsd

--- e2e-test/cypress/support/step_definitions/runner/i_upload_a_file_and_continue.js
+++ e2e-test/cypress/support/step_definitions/runner/i_upload_a_file_and_continue.js
@@ -1,7 +1,7 @@
 import {When} from "@badeball/cypress-cucumber-preprocessor";
 
 When("I upload the file {string} wait till upload and then save and continue with {string}", (filename, isContinue) => {
-  cy.get("input[type=file]").attachFile(filename);
+  cy.get("input[type=file]").attachFile(`../../../../e2e-test/cypress/fixtures/${formName}`);
   cy.wait(2000)
   if (isContinue && isContinue==="true") {
     cy.findByRole("button", {name: /continue/i}).click();
