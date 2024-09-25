4632a5ec8253289178f00a614a53095ccd666c16
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-09-25 16:22:02 +0100

    add callback handler

--- runner/src/server/services/AdapterStatusService.ts
+++ runner/src/server/services/AdapterStatusService.ts
@@ -1,6 +1,7 @@
 import {StatusService} from "../../../../digital-form-builder/runner/src/server/services";
 import {AdapterCacheService} from "./AdapterCacheService";
 import {HapiServer} from "../types";
+import {HapiRequest} from "../../../../digital-form-builder/runner/src/server/types";
 
 
 export class AdapterStatusService extends StatusService {
@@ -15,4 +16,13 @@ export class AdapterStatusService extends StatusService {
         this.cacheService = adapterCacheService;
     }
 
+    //@ts-ignore
+    async outputRequests(request: HapiRequest) {
+        const state = await this.cacheService.getState(request);
+        const {callback} = state;
+        if (callback && (callback !== "undefined" || callback !== undefined)) {
+            await super.outputRequests(request);
+        }
+        this.logger.info(`[AdapterStatusService] Cannot save data by using callback url ignore save call back is ${callback}`);
+    }
 }
