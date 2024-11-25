5bb2b6f16e3ab0fb0285594d1c1252f1935996fd
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-11-25 15:43:15 +0000

    FS-4821: loggin configuration changes

--- runner/src/server/plugins/logging.ts
+++ runner/src/server/plugins/logging.ts
@@ -1,4 +1,58 @@
-import pluginLogging from "../../../../digital-form-builder/runner/src/server/plugins/logging";
+import pino from "hapi-pino";
+import {config} from "./utils/AdapterConfigurationSchema";
 
-pluginLogging.options.ignoreFunc = (_options, request) => request.path.startsWith("/assets")
-export const pluginLog: any = pluginLogging
+export default {
+    plugin: pino,
+    options: {
+        prettyPrint: false,
+        level: config.logLevel,
+        base: null,
+        formatters: {
+            level: (label) => {
+                return {severity: label.toUpperCase()}; // Align severity levels for CloudWatch
+            },
+        },
+        messageKey: "message",
+        customProps: (request) => ({
+            requestId: request.info.id, // Include request ID
+            userAgent: request.headers["user-agent"], // Add user agent for context
+            route: request.route.path, // Include route information
+        }),
+        serializers: {
+            req: () => undefined,
+            res: () => undefined,
+        },
+        hooks: {
+            logMethod(inputArgs, method) {
+                let [obj, ...rest] = inputArgs;
+                if (typeof obj === "object") {
+                    if (obj.req) {
+                        //@ts-ignore
+                        obj = {
+                            ...obj,
+                            requestId: obj.req.info.id,
+                            userAgent: obj.req.headers["user-agent"],
+                            route: obj.req.route.path,
+                        };
+                        delete obj.req
+                        delete obj.res
+                    }
+                    delete obj.req
+                    delete obj.res
+                    return method.apply(this, [obj, ...rest]);
+                }
+                return method.apply(this, inputArgs);
+            },
+        },
+        redact: {
+            paths: config.logRedactPaths || ["req.headers.authorization"],
+            censor: null,
+        },
+        ignoreFunc: (_options, request) =>
+            request.path.startsWith("/assets"),
+        debug: config.isDev,
+        logRequestStart: config.isDev,
+        logRequestComplete: config.isDev,
+    },
+};
+
