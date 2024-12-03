774c34ccc64b444c84279e8bde82a7ab110b1f20
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-12-03 17:37:39 +0000

    loggin adding addressing review comments

--- runner/src/server/plugins/logging.ts
+++ runner/src/server/plugins/logging.ts
@@ -4,7 +4,7 @@ import {config} from "./utils/AdapterConfigurationSchema";
 export default {
     plugin: pino,
     options: {
-        prettyPrint: false,
+        prettyPrint: config.logPrettyPrint === "true" || config.logPrettyPrint === true,
         level: config.logLevel,
         base: null,
         formatters: {
@@ -13,52 +13,9 @@ export default {
             },
         },
         messageKey: "message",
-        customProps: (request) => ({
-            requestId: request.info.id, // Include request ID
-            userAgent: request.headers["user-agent"], // Add user agent for context
-            route: request.route.path, // Include route information
-        }),
-        serializers: {
-            req: () => undefined,
-            res: () => undefined,
-        },
-        hooks: {
-            logMethod(inputArgs, method) {
-                let [obj, ...rest] = inputArgs;
-                if (typeof obj === "object") {
-                    if (obj.req) {
-                        //@ts-ignore
-                        obj = {
-                            ...obj,
-                            requestInfoId: obj.req.info.id,
-                            requestId: obj.req.yar.id,
-                            userAgent: obj.req.headers["user-agent"]
-                        };
-                        if (obj.req.path) {
-                            obj.path = obj.req.path;
-                        }
-                        if (obj.req.query) {
-                            obj.query = obj.req.query;
-                        }
-                        if (obj.req.method) {
-                            obj.method = obj.req.method.toUpperCase();
-                        }
-                        if (obj.req.yar.id) {
-                            obj.requestId = obj.req.yar.id;
-                        }
-                        delete obj.req
-                        delete obj.res
-                    }
-                    delete obj.req
-                    delete obj.res
-                    return method.apply(this, [obj, ...rest]);
-                }
-                return method.apply(this, inputArgs);
-            },
-        },
         redact: {
-            paths: config.logRedactPaths || ["req.headers.authorization"],
-            censor: null,
+            paths: config.logRedactPaths,
+            remove: true,
         },
         ignoreFunc: (_options, request) =>
             request.path.startsWith("/assets"),
