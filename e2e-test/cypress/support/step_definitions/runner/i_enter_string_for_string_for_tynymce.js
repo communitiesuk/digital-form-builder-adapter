c7f4fe4fc2c5c181666142c49fcdf18908b541f3
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-12-04 12:00:40 +0000

    adding e2e fix

--- e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
+++ e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
@@ -1,10 +1,19 @@
 import {Then, When} from "@badeball/cypress-cucumber-preprocessor";
 
 When('I enter {string} into the TinyMCE editor {string}', (text, fieldName) => {
-  cy.get(`#${fieldName}`, { timeout: 10000 }).should('exist').then((textarea) => {
-    textarea[0].value = text;
-    const inputEvent = new Event('input', { bubbles: true, cancelable: true });
-    textarea[0].dispatchEvent(inputEvent);
+  cy.window().then((win) => {
+    return new Cypress.Promise((resolve) => {
+      const editor = win.tinymce.get(fieldName);
+      if (editor) resolve();
+    });
+  });
+  cy.get(`#${fieldName}_ifr`, {timeout: 10000}).should('exist').then(($iframe) => {
+    const iframeBody = $iframe.contents().find('body'); // Access the iframe's body
+    cy.wrap(iframeBody).clear().type(text); // Clear and type the text
+  });
+  cy.get(`#${fieldName}_ifr`).then(($iframe) => {
+    const iframeBody = $iframe.contents().find('body');
+    cy.wrap(iframeBody).should('contain.text', text); // Assert the content
   });
 });
 
