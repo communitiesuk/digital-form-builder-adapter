d9f0991d200a4ecbe8fa19569b0b24363173b7e0
Hamza Khalid <hamza.khalid@levellingup.gov.uk>
2024-12-04 11:04:47 +0000

    FLS-823: Add redirect to eligible round in eligibility result URL in ConfirmPageController.ts logic responsible for handling eligibility journey

--- runner/src/server/plugins/engine/page-controllers/ConfirmPageController.ts
+++ runner/src/server/plugins/engine/page-controllers/ConfirmPageController.ts
@@ -21,7 +21,23 @@ export class ConfirmPageController extends SummaryPageController {
             const state = await adapterCacheService.getState(request);
             const fund_name = state["metadata"]["fund_name"];
             const round_name = state["metadata"]["round_name"];
-            return redirectTo(request, h, config.eligibilityResultUrl + "/" + fund_name + "/" + round_name);
+
+            let question_answer_short_code;
+
+            // get the question_answer_short_code (round shortname to redirect to) if it was included in the funds eligibility questions
+            for (let [, section] of Object.entries(state)) {
+                if (section && section["redirectToEligibleRound"]) {
+                    question_answer_short_code = section["redirectToEligibleRound"];
+                }
+            }
+
+            const url = new URL(`${config.eligibilityResultUrl}/${fund_name}/${round_name}`)
+
+            if (question_answer_short_code) {
+                url.searchParams.set('redirect_to_eligible_round', question_answer_short_code)
+            }
+
+            return redirectTo(request, h, url.href)
         };
     }
 }
