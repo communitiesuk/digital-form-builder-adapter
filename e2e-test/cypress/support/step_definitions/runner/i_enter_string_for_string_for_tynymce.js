6800682fabc3b4e86ee3f2cd7ce6e33ab37dc0a3
Nuwan Samarassinghe <nuwansamarasinghe100@gmail.com>
2024-12-04 12:22:40 +0000

    Fix e2e (#80)
    * adding e2e fix
    
    * adding e2e fix

--- e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
+++ e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
@@ -1,7 +1,20 @@
 import {Then, When} from "@badeball/cypress-cucumber-preprocessor";
 
 When('I enter {string} into the TinyMCE editor {string}', (text, fieldName) => {
-  cy.get(`#${fieldName}:hidden`).invoke('val', text);
+  cy.window().then((win) => {
+    return new Cypress.Promise((resolve) => {
+      const editor = win.tinymce.get(fieldName);
+      if (editor) resolve();
+    });
+  });
+  cy.get(`#${fieldName}_ifr`, {timeout: 10000}).should('exist').then(($iframe) => {
+    const iframeBody = $iframe.contents().find('body'); // Access the iframe's body
+    cy.wrap(iframeBody).clear().type(text); // Clear and type the text
+  });
+  cy.get(`#${fieldName}_ifr`).then(($iframe) => {
+    const iframeBody = $iframe.contents().find('body');
+    cy.wrap(iframeBody).should('contain.text', text); // Assert the content
+  });
 });
 
 Then("I checked for any errors {string} exists for TinyMCE editor", (error) => {
