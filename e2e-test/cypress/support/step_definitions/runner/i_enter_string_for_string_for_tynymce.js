414ff3c4c9af921d94eaa0b5e50d90871bedd8e3
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-12-04 11:24:44 +0000

    adding e2e fix

--- e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
+++ e2e-test/cypress/support/step_definitions/runner/i_enter_string_for_string_for_tynymce.js
@@ -1,7 +1,11 @@
 import {Then, When} from "@badeball/cypress-cucumber-preprocessor";
 
 When('I enter {string} into the TinyMCE editor {string}', (text, fieldName) => {
-  cy.get(`#${fieldName}:hidden`).invoke('val', text);
+  cy.get(`#${fieldName}`, { timeout: 10000 }).should('exist').then((textarea) => {
+    textarea[0].value = text;
+    const inputEvent = new Event('input', { bubbles: true, cancelable: true });
+    textarea[0].dispatchEvent(inputEvent);
+  });
 });
 
 Then("I checked for any errors {string} exists for TinyMCE editor", (error) => {
