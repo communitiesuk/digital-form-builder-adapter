d60d3d4ec9499d20ec03b61a24e4b478e80e721d
Hamza Khalid <hamza.khalid@levellingup.gov.uk>
2024-12-09 12:01:50 +0000

    - added new question in the eligiblity form - refactored eligiblity form - old hsra folder removed - removed non eligiblity - added different folders for vr and rp

--- runner/src/server/plugins/engine/page-controllers/ConfirmPageController.ts
+++ runner/src/server/plugins/engine/page-controllers/ConfirmPageController.ts
@@ -4,6 +4,8 @@ import {redirectTo} from "../util/helper";
 import {AdapterFormModel} from "../models";
 import {SummaryPageController} from "./SummaryPageController";
 
+const REDIRECT_TO_ELIGIBLE_ROUND_QUESTION_KEY = "redirectToEligibleRound";
+
 export class ConfirmPageController extends SummaryPageController {
     // Controller to add confirm and continue button
     // @ts-ignore
@@ -21,7 +23,22 @@ export class ConfirmPageController extends SummaryPageController {
             const state = await adapterCacheService.getState(request);
             const fund_name = state["metadata"]["fund_name"];
             const round_name = state["metadata"]["round_name"];
-            return redirectTo(request, h, config.eligibilityResultUrl + "/" + fund_name + "/" + round_name);
+            let eligibleRoundRedirectAnswerShortCode;
+
+            // get the eligibleRoundRedirectAnswerShortCode (round to redirect to) if it was included in the funds eligibility questions
+            for (let [, section] of Object.entries(state)) {
+                if (section && section[REDIRECT_TO_ELIGIBLE_ROUND_QUESTION_KEY]) {
+                    eligibleRoundRedirectAnswerShortCode = section[REDIRECT_TO_ELIGIBLE_ROUND_QUESTION_KEY];
+                }
+            }
+
+            const url = new URL(`${config.eligibilityResultUrl}/${fund_name}/${round_name}`)
+
+            if (eligibleRoundRedirectAnswerShortCode) {
+                url.searchParams.set('redirect_to_eligible_round', eligibleRoundRedirectAnswerShortCode)
+            }
+
+            return redirectTo(request, h, url.href)
         };
     }
 }
