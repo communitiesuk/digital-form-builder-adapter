394aaaa752a51c4027614ee24865f28470c8f4d4
Nuwan Samarassinghe <nuwansamarasinghe100@gmail.com>
2025-01-07 17:32:37 +0000

    fix review comments and fix merge conflicts (#95)
    adding Sentry monitoring for runner

--- runner/src/index.ts
+++ runner/src/index.ts
@@ -1,9 +1,10 @@
 import createServer from "./server";
 
 createServer({})
-  .then((server) => server.start())
-  .then(() => process.send && process.send("online"))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    .then((server) => server.start())
+    .then(() => process.send && process.send("online"))
+    .then(() => console.log("*** SERVER STARTED ***"))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
