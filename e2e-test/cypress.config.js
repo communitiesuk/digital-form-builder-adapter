5a9f29a60ba555fb36f57554bb988215db9cc52e
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-07-09 11:37:30 +0100

    Fix e2e test to have malware detection changes

--- e2e-test/cypress.config.js
+++ e2e-test/cypress.config.js
@@ -2,6 +2,7 @@ const {defineConfig} = require("cypress");
 const webpack = require("@cypress/webpack-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const path = require("path");
+const AWS = require('aws-sdk');
 
 
 console.log("****************** starting e2e ******************")
@@ -33,6 +34,45 @@ async function setupNodeEvents(on, config) {
       },
     })
   );
+
+  on('task', {
+    async tagAllS3Objects({ tags }) {
+      console.log('tagAllS3Objects task started with tags:', tags);
+      const s3 = new AWS.S3({
+        endpoint: 'http://localhost:4566',
+        s3ForcePathStyle: true,
+        accessKeyId: 'FSDIOSFODNN7EXAMPLE',
+        secretAccessKey: 'fsdlrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+        region: 'eu-west-2',
+      });
+      try {
+        const listedObjects = await s3.listObjectsV2({ Bucket: 'fsd-bucket' }).promise();
+        if (!listedObjects.Contents || listedObjects.Contents.length === 0) {
+          const msg = `No objects found in bucket fsd-bucket`;
+          console.log(msg);
+          return msg;
+        }
+        const taggingPromises = listedObjects.Contents.map((obj) => {
+          console.log(`Tagging object: ${obj.Key}`);
+          return s3.putObjectTagging({
+            Bucket: 'fsd-bucket',
+            Key: obj.Key,
+            Tagging: {
+              TagSet: Object.entries(tags).map(([Key, Value]) => ({ Key, Value })),
+            },
+          }).promise();
+        });
+        await Promise.all(taggingPromises);
+        const successMsg = `${listedObjects.Contents.length} object(s) in fsd-bucket tagged successfully.`;
+        console.log(successMsg);
+        return successMsg;
+      } catch (error) {
+        console.error("Error tagging S3 objects:", error);
+        throw error;
+      }
+    },
+  });
+
   // Make sure to return the config object as it might have been modified by the plugin
   return config;
 }
