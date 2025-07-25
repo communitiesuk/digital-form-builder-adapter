d4487f79e0d87590fd9f79bf3853fdef898db293
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-07-25 12:40:56 +0100

    Fix e2e test with the error summary title change

--- e2e-test/cypress/support/step_definitions/runner/i_see_the_error_string_for_string.js
+++ e2e-test/cypress/support/step_definitions/runner/i_see_the_error_string_for_string.js
@@ -1,8 +1,29 @@
-import { When } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I see the error {string} for {string} component", (error, fieldName) => {
-  cy.findByText("Fix the following errors");
+  cy.findByText("There is a problem");
   cy.findByRole("link", { name: error });
+});
+
+When("I see the error {string} for component {string}", (error, fieldName) => {
+  cy.findByText("There is a problem");
+  cy.findByRole("link", { name: error });
+  cy.findByRole("group", { description: `Error: ${error}` });
+});
+
+Then(
+  "I see the date parts with a error string {string} for {string}",
+  (error, fieldName) => {
+    cy.findByText("There is a problem");
+    cy.get(`#${fieldName}-error`).within(() => {
+      cy.findByText(error, { exact: false });
+    });
+  }
+);
+
+Then("I see the date parts with error {string}", (error) => {
+  cy.findByText("There is a problem");
+  cy.findByRole("link", { name: error, exact: false });
 });
 
 When("I see the error {string} for {string} component with problem title", (error, fieldName) => {
