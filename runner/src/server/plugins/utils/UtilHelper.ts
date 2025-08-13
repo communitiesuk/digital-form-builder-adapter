74bd1751d3c621cb300ac258f220b6b4b69aef20
Hamza Khalid <hamza.khalid@levellingup.gov.uk>
2025-08-13 14:18:59 +0100

    - Added TODO for adding Welsh translation for "Go back to previous page"

--- runner/src/server/plugins/utils/UtilHelper.ts
+++ runner/src/server/plugins/utils/UtilHelper.ts
@@ -11,6 +11,7 @@ export class UtilHelper {
       case BackLinkType.Eligibility:
         return isWelsh ? "Yn ôl at eich ceisiadau" : "Back to your applications";
       case BackLinkType.PreviousPage:
+        // TODO: Add Welsh translation for "Go back to previous page"
         return isWelsh ? "" : "Go back to previous page";
       case BackLinkType.ApplicationOverview:
         return isWelsh ? "Yn ôl i'r trosolwg o'r cais" : "Go back to application overview";
