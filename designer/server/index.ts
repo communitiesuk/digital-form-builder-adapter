4219d1fa12db5d3ba7c4a877d420169045a6ba40
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-08-28 12:53:24 +0100

    FLS-1473: Yarn configuration changes

--- designer/server/index.ts
+++ designer/server/index.ts
@@ -4,6 +4,7 @@ import {createServer} from "./createServer";
 createServer()
     .then((server) => server.start())
     .then(() => process.send && process.send("online"))
+    .then(() => console.log("*** SERVER STARTED ***"))
     .catch((err) => {
         console.error(err);
         process.exit(1);
